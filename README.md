8c852413d724ab142ca441d5b97fc65451988728
渔火Arcadia <85126209+yhArcadia@users.noreply.github.com>
2022-09-03 18:00:50 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+#### 此为Yunzai-Bot云崽QQ机器人的插件，需要依赖云崽机器人来实现功能
+# 插件列表
+* ### 原神角色语音合成（v2）（v3）
+用于实现文字转语音。
+![音色列表](./resources/soundlist.jpg)
+
+<br>
+<br>
+
+* ### 搓新人头像（v3）
+有人加群或退群时生成相应的头像表情包，**依赖[宵鸟py-plugin](https://gitee.com/realhuhu/py-plugin)的头像表情包模块**
+![搓新人头像](./resources/搓新人头像.png)
+
+
+
+
+<br>
+<br>
+
+## 使用方法
+根据你的云崽版本下载对应版本插件，V2放进/lib/example文件夹，V3放进plugins/example文件夹
+如何**下载单个插件**：点击"raw"（gitee中是“原始数据”）查看该插件源文件，将网页另存问即可；也可以点击copy代码，粘贴至本地.js文件中
+![下载单个插件](./resources/如何下载单个插件.png)
+<br>
+<br>
+
+### 其他
+* [Yunzai-Bot](https://github.com/Le-niao/Yunzai-Bot)
+* Yunzai-Bot插件库：[☞github](https://github.com/yhArcadia/Yunzai-Bot-plugins-index) [☞gitee](https://gitee.com/yhArcadia/Yunzai-Bot-plugins-index)
+* QQ交流群：134086404 （璃月幼稚园）
---
+++ 搓新人头像（v3）.js
@@ -0,0 +1,168 @@
+import plugin from '../../lib/plugins/plugin.js';
+import { segment } from "oicq";
+import { FrameToFrame, createEvent } from "../py-plugin/core/client/client.js";
+import { imageUrlToBuffer } from "../py-plugin/core/util/transform.js";
+
+export class newcomer extends plugin {
+  constructor() {
+    super({
+      name: '欢迎新人',
+      dsc: '新人入群欢迎',
+      /** https://oicqjs.github.io/oicq/#events */
+      event: 'notice.group.increase',
+      priority: 4000
+    })
+  }
+
+  /** 接受到消息都会执行一次 */
+  async accept() {
+
+    /** 定义入群欢迎内容 */
+    let msg = '欢迎新人！';
+    /** 冷却cd 30s */
+    let cd = 30;
+
+    if (this.e.user_id == Bot.uin) { return }
+
+    /** cd */
+    let key = `Yz:newcomers:${this.e.group_id}`;
+    if (await redis.get(key)) { return }
+    redis.set(key, '1', { EX: cd });
+
+    /** 回复 */
+    await this.reply([
+      segment.at(this.e.user_id),
+      // segment.image(),
+      msg
+    ])
+
+    // 搓新人头像===============================\\
+    let increasemem = ["_jiujiu", "_tightly", "_anyasuki", "_petpet", "_pat", "_play"];
+    let imgbf = await imageUrlToBuffer(`https://q1.qlogo.cn/g?b=qq&s=0&nk=${this.e.user_id}`);
+    FrameToFrame({
+      _package: "petpet",
+      _handler: increasemem[Math.floor(Math.random() * increasemem.length)],
+      params: {
+        // event: await createEvent(this.e),
+        event: {
+          // sender: this.e.user_id,
+          sender: {
+            qq: this.e.user_id.toString(),
+            name: "233",
+            card: "233",
+            sex: "233",
+            age: "233",
+            area: "233",
+            level: "233",
+            role: "233",
+            title: "233",
+          },
+          atList: [{
+            qq: this.e.user_id.toString(),
+            name: this.e.user_id.toString(),
+          }],
+          imageList: [imgbf],
+          msg: "",
+          group: null
+        },
+        message: "欢迎欢迎~"
+      },
+      onData: (error, response) => {
+        if (error) {
+          console.error(error.details);
+        } else {
+          this.reply(segment.image(response.image));
+        }
+      },
+    });
+
+    // // 生成搓新人的头像表情包
+    // let url = `https://api.dlut-cc.live/emoji/?flag=_&qq=${this.e.user_id}&target=${this.e.user_id}&group=${this.e.group_id}&args=${encodeURI("搓")}&master=${cfg.masterQQ[0]}`
+    // console.log(url);
+    // //let response = await fetch(url); //调用接口获取数据
+    // let response = await axios.get(url, { timeout: 20000 }); //调用接口获取数据
+    // //const res = await response.json(); //结果json字符串转对象
+    // const res = await response.data; //结果json字符串转对象
+    // if (res.success == "true") {
+    //   await this.reply([segment.at(this.e.user_id), segment.text(msg), segment.image(res.url)])
+    // }
+    // else {
+    //   await this.reply([segment.at(this.e.user_id),segment.text(msg)])
+    // }
+  }
+}
+
+export class outNotice extends plugin {
+  constructor() {
+    super({
+      name: '退群通知',
+      dsc: 'xxx永远离开了我们',
+      event: 'notice.group.decrease'
+    })
+
+    /** 退群提示词 */
+    this.tips = '永远离开了我们。。'
+  }
+
+  async accept() {
+    if (this.e.user_id == Bot.uin) {
+      return
+    }
+    let name, msg;
+    if (this.e.member) {
+      name = this.e.member.card || this.e.member.nickname
+    }
+
+    if (name) {
+      msg = `${name}(${this.e.user_id}) ${this.tips}`
+    } else {
+      msg = `${this.e.user_id} ${this.tips}`
+    }
+
+
+
+    // 缅怀旧人头像=============================================================================\\
+    let decreasemem = ["_hammer", "_thump", "_rip", "_eat"];
+    let imgbf = await imageUrlToBuffer(`https://q1.qlogo.cn/g?b=qq&s=0&nk=${this.e.user_id}`);
+    FrameToFrame({
+      _package: "petpet",
+      _handler: decreasemem[Math.floor(Math.random() * decreasemem.length)],
+      params: {
+        // event: await createEvent(this.e),
+        event: {
+          // sender: this.e.user_id,
+          sender: {
+            qq: this.e.user_id.toString(),
+            name: "233",
+            card: "233",
+            sex: "233",
+            age: "233",
+            area: "233",
+            level: "233",
+            role: "233",
+            title: "233",
+          },
+          atList: [{
+            qq: this.e.user_id.toString(),
+            name: this.e.user_id.toString(),
+          }],
+          imageList: [imgbf],
+          msg: "",
+          group: null
+        },
+        message: "缅怀"
+      },
+      onData: (error, response) => {
+        if (error) {
+          console.error(error.details);
+        } else {
+          this.reply(segment.image(response.image));
+        }
+      },
+    });
+    // =======================================================================//
+
+    logger.mark(`[退出通知]${this.e.logText} ${msg}`);
+    await this.reply(msg);
+  }
+}
